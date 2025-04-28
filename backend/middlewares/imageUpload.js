e244c00b05c7c009186b4600c51588d4c98a584a
Giulliano <tharsoweb@gmail.com>
2025-04-28 16:36:14 -0300

    upload de imagens

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,35 @@
+const multer = require("multer")
+const path = require("path")
+
+// Destination to store
+const imageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = ""
+
+        if (req.baseUrl.includes("users")) {
+            folder = "users"
+        } else if (req.baseUrl.includes("photos")) {
+            folder = "photos"
+        }
+
+        cb(null, `uploads/${folder}/`)
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter: (req, file, cb) => {
+        
+        // Upload only png and jpeg formats
+        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+            return cb(new Error("Formato inválido (Envie png ou jpeg)"))
+        }
+
+        cb(null, true)
+    }
+})
+
+module.exports = { imageUpload }
